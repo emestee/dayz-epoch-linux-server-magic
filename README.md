e888da2a813443f66bd7cfb048bf9692fa7aa0b9
Michael Stolovitzsky <emestee@catnarok.net>
2014-08-19 21:24:36 +0300

    WIP initial commit

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-dayz-epoch-linux-server-magic
-=============================
+Automated DayZ Epoch Linux server _magic_ installer
+===========================================
+
+Current Epoch version: *1.0.5.1*
+
+Current Arma 2 AO server engine: *1.63.112555* (e91456595c3380f853097e0c767e1d0e)
+
+Current Arma 2 AO beta data files: *1.63.122548*
+
+What is this?
+-------------
+
+This is a hands-free make-based automated installer for Dayz Epoch
+game server. It automatically downloads all the required dependencies,
+including the game binaries from Steam, and composes them into a
+runnable server. I wrote it so that nobody else has to undergo the
+same terrifying manual procedure I did which involved waiting for days
+to finish uploading the enormous game files to a VPS off a slow home
+DSL line.
+
+Here is what it does exactly:
+
+* Downloads the game data files from Steam
+* Downloads the current server binary from BIS website
+* Downloads the necessary PBOs
+* Downloads the Linux hive port by denisio
+* Composes all of the above to create a working server environment
+* Creates a single, default database instance and fills it with the default dump
+
+On a DigitalOcean VPS, the whole process takes 10 minutes.
+What do I need?
+---------------
+
+* A Linux machine with a recent (2.16+) glibc, Debian Wheezy works
+* About 20Gb disk space and 2Gb RAM (the server will probably run with less; haven't tried)
+* A Steam account with Arma II and Arma II: AO
+
+How do I run this?
+------------------
+
+* If possible, create a separate system user, e.g. `epoch`, and perform all operations in and as it
+* Clone this repository with `git clone git@github.com:emestee/dayz-epoch-linux-server-magic.git`
+* On Debian, run `packages.sh` **as root** to install the prerequisites. Otherwise look at the content of the file
+  and install the equivalent packages.
+* Copy the `CONFIGURATION-dist` file to `CONFIGURATION` and edit it. At the bare minimum insert your steam login and password.
+  If you have Steam Guard enabled, at the download stage it will ask you for a guard code that you receive via the email
+  associated with your account.  **Important**: after the installation is finished, remove your login/password from this file.
+  Never leave it on a server.
+* Run `./install.sh all`
+
+Occasionally Steam would barf and the download will stop. Just try again.
+
+After the whole thing finished running, hopefully without failure, you
+will end up with a preconfigured server in `../epoch`.
+
+It failed, what do I do?
+------------------------
+
+It's probably my fault! This script is a hack. My goal is to make the
+procedure painless. Let me know and I will try to help you fixing it.
+
+If something was wrong with your configuration, adjust it and perform
+`./install.sh reset` and then `./install.sh all` again.
+
+Try to figure out what went wrong. The script is organized into
+stages; reset as above, look at the `all` procedure, and execute every stage manually
+via `./install.sh <stage>`.
+
+What's next?
+------------
+
+* Check the game server will run correctly by going to the server directory and running `./epoch.sh`
+* Log in to the game as a player, run around, kill some zombies, log off, log on again and see that everything is fine (you're 
+  spawned back where you were and your stuff isn't missing)
+* Configure your server (hostname, motd, slots, password, etc). Note that the current server PBO has missions in Russian
+  because that's how denisio publishes it. The recipe how to fix it can be found on epochmod.com forums.
+* Run with `./restarter.pl`
+* After everything is in order, delete this installer, or at the very least, **remove your steam user and password from the `CONFIGURATION` file**
+
+Thanks
+------
+
+* BIS for an amazing game and the engine
+* Epoch mod developers 
+* denisio for porting the hive mechanism to Linux
+* DeanReid for help
---
+++ install.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+. CONFIGURATION
+
+fail () {
+    echo "***FAILED*** $1"
+    exit 1
+}
+
+help () {
+    echo "Syntax: ./install.sh <command>"
+    echo
+    echo "Available commands are:"
+    echo
+    echo "* all - Download and install everything; use this, everything else is steps in case stuff breaks"
+    echo "* dl_epoch - Download Epoch client files (torrent)"
+    echo "* dl_steamcmd - Download and unpack SteamCmd"
+    echo "* dl_game - Download ARMA II, ARMA II: OA and the beta packages using SteamCMD"
+    echo "* dl_server - Download Linux server binaries"
+    echo "* dl_tools - Download denisio's server tools"
+    echo "* compose - Combine everything into a working server directory"
+    echo "* sql - Initialize the SQL database"
+    echo "* clean - Remove unneeded garbage from the server directory"
+    echo "* reset - Start from scratch (destroys the server directory and the SQL database)"
+    echo
+    echo "Server will be installed into ${SERVER_PATH}"
+}
+
+
+
+all () {
+    [ -d "$SERVER_PATH" ] && fail "Safety: server directory $SERVER_PATH already exists, this procedure will destroy it; exiting."
+
+    mkdir -p $CACHE $SERVER_PATH 
+
+    echo "Installing Steam"
+    dl_steamcmd
+    echo "Downloading and installing the game - long"
+    dl_game
+    echo "Downloading the server binaries"
+    dl_server
+    echo "Downloading the server tools"
+    dl_tools
+    echo "Downloading and installing the Epoch client file via bittorrent - long"
+    dl_epoch
+    echo "Here be dragons! Composing the rest."
+    compose
+    echo "Initializing the database"
+    sql
+    echo "Cleanup"
+    clean
+    echo "All done!"
+}
+
+dl_steamcmd () {
+    curl -s -o $CACHE/steamcmd_linux.tar.gz $STEAMCMD_URL || fail "Unable to download SteamCmd"
+    mkdir steamcmd -p
+    tar xzf $CACHE/steamcmd_linux.tar.gz -C steamcmd
+}
+
+dl_game () {
+    steamcmd/steamcmd.sh +login $STEAM_USERNAME $STEAM_PASSWORD +runscript ../script.steam || fail "SteamCmd game installation failed"
+}
+
+dl_server () {
+    a2oa_tarball=$(basename $ARMA2_SERVER_URL)
+    curl -s -o $CACHE/${a2oa_tarball} $ARMA2_SERVER_URL || fail "Unable to download the server tarball from $ARMA2_SERVER_URL"
+
+    mkdir -p arma2-server
+    tar xjf $CACHE/${a2oa_tarball} -C arma2-server || fail "Unable to extract the server tarball"
+}
+
+dl_tools () {
+    git submodule init
+    git submodule update || exit "Failed to update git submodules"
+}
+
+dl_epoch () {
+    curl -o $CACHE/${EPOCH_CLIENT_TARBALL}.torrent $EPOCH_CLIENT_URL 
+    pushd $CACHE
+    unworkable ${EPOCH_CLIENT_TARBALL}.torrent > epoch-client-torrent.log
+    popd
+}
+
+extract_epoch () {
+    echo Installing Epoch client files
+    7zr x -o${SERVER_PATH} $CACHE/${EPOCH_CLIENT_TARBALL} > /dev/null || fail "Unable to extract ${EPOCH_CLIENT_TARBALL}, file corrupt/disk full?"
+}
+
+clean () {
+    find $SERVER_PATH -iname '*.pdf' -delete
+    find $SERVER_PATH -iname '*.exe' -delete
+    find $SERVER_PATH -iname '*.vdf' -delete
+    find $SERVER_PATH -iname '*.cmd' -delete
+    find $SERVER_PATH -iname '*.dll' -delete
+    find $SERVER_PATH -iname '*.bat' -delete
+    rm -rf $SERVER_PATH/*.txt $SERVER_PATH/dll $SERVER_PATH/besetup $SERVER_PATH/directx
+
+    find $SERVER_PATH -type f -exec chmod a-x {} \;
+    chmod u+x $SERVER_PATH/epoch $SERVER_PATH/*.pl $SERVER_PATH/*.sh
+}
+
+compose () {
+    extract_epoch
+
+    echo "Applying downcasing, dog bless"
+    find $SERVER_PATH -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
+
+    echo "Installing denisio's Linux tools"
+    cp Dayz-Epoch-Linux-Server/*.pl $SERVER_PATH
+    cp Dayz-Epoch-Linux-Server/*.sh $SERVER_PATH
+    cp -r Dayz-Epoch-Linux-Server/cache $SERVER_PATH
+    cp -r Dayz-Epoch-Linux-Server/cfgdayz $SERVER_PATH
+    cp -r Dayz-Epoch-Linux-Server/@dayz_epoch_server $SERVER_PATH
+    cp -r Dayz-Epoch-Linux-Server/expansion $SERVER_PATH
+    cp -r Dayz-Epoch-Linux-Server/keys $SERVER_PATH
+    cp -r Dayz-Epoch-Linux-Server/mpmissions $SERVER_PATH
+
+    echo "Installing server binaries"
+    cp arma2-server/server $SERVER_PATH/epoch
+    cp -r arma2-server/expansion $SERVER_PATH
+}
+
+sql () {
+    mysqladmin -u${MYSQL_ADMIN_USER} -p${MYSQL_ADMIN_PASSWORD} create epoch || fail "Unable to create the database; invalid user/password?"
+    mysql -u${MYSQL_ADMIN_USER} -p${MYSQL_ADMIN_PASSWORD} -e "GRANT ALL PRIVILEGES ON epoch.* TO 'dayz'@'localhost' IDENTIFIED BY 'dayz';"
+    cat Dayz-Epoch-Linux-Server/database.sql Dayz-Epoch-Linux-Server/v1042update.sql Dayz-Epoch-Linux-Server/v1042a_update.sql Dayz-Epoch-Linux-Server/v1051update.sql | mysql -udayz -pdayz epoch
+}
+
+reset () {
+    rm -rf $SERVER_PATH
+    yes|mysqladmin -u${MYSQL_ADMIN_USER} -p${MYSQL_ADMIN_PASSWORD} drop epoch > /dev/null
+    
+}
+[ "$1" = "" ] && { 
+    help
+    exit
+}
+
+$*
+echo End.
+exit 0
