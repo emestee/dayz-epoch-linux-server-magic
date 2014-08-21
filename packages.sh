e0e4c5edd3b9892017e76b9de1d7d9c247a24b08
Michael Stolovitzsky <emestee@dcashier.com>
2014-08-21 19:26:11 +0000

    silly emestee, test your scripts

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-apt-get install perl screen mysql-server mysql-client libjson-xs-perl libdbd-mysql-perl git unknowable
+apt-get install perl screen mysql-server mysql-client libjson-xs-perl libdbd-mysql-perl git unworkable
 
-[ $(uname -m) == "x86_64" ] && apt-get install lib32stdc++6
+[ "$(uname -m)" == "x86_64" ] && apt-get install lib32stdc++6
