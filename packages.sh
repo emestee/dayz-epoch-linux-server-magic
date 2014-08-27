65173cd77b971a992cdbcaa829cf66b6a2795d03
rezor92 <rezor92@googlemail.com>
2014-08-27 13:19:43 +0200

    Update packages.sh
    packages needed
    + replace torrent client cause the old one is crappy

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-apt-get install perl screen mysql-server mysql-client libjson-xs-perl libdbd-mysql-perl git unworkable
+apt-get install perl screen mysql-server mysql-client libjson-xs-perl libdbd-mysql-perl git ctorrent curl p7zip
 
 [ "$(uname -m)" == "x86_64" ] && apt-get install lib32stdc++6
