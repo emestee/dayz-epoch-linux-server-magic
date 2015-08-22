8802efa38079828c6a88ac010c5efe3c69ea7c3c
Michael Stolovitzsky <emestee@catnarok.net>
2015-08-22 14:34:25 -0400

    wrong shell

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 apt-get install perl screen mysql-server mysql-client libjson-xs-perl libdbd-mysql-perl git ctorrent curl p7zip
 
-[ "$(uname -m)" == "x86_64" ] && apt-get install lib32stdc++6
+[ `uname -m` == 'x86_64' ] && apt-get install lib32stdc++6
